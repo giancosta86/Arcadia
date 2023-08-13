9f6b365ef96b4493690f4ed95d42149704dc2691
Gianluca Costa <gianluca@gianlucacosta.info>
2023-08-13 23:09:58 +0200

    Simplify app component declaration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import { AppProps } from "next/app";
 import "@giancosta86/hermes-react/dist/index.css";
 import "../styles/index.scss";
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-
-export default MyApp;
