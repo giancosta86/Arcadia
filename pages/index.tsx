e4e9d7b038936e390eed4b7501e17bf22a3a6b62
Gianluca Costa <gianluca@gianlucacosta.info>
2023-09-05 15:37:54 +0200

    Revise styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,11 @@ import { ViewComposition, Site } from "../model";
 export default function Home() {
   return (
     <Layout>
-      <div>
-        <CompositionList
-          compositions={Site.compositionRepository
-            .findAll()
-            .map(composition => ViewComposition.from(composition))}
-        />
-      </div>
+      <CompositionList
+        compositions={Site.compositionRepository
+          .findAll()
+          .map(composition => ViewComposition.from(composition))}
+      />
     </Layout>
   );
 }
